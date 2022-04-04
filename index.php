861d03935c69726d8453939aed82089413fee16c
linn-khant <linkhantnyinyi242@gmail.com>
2022-04-04 02:23:53 -0700

    add slogan

--- index.php
+++ index.php
@@ -303,13 +303,20 @@
          <button type="submit" name="submit" value="submit" class="btn btn-primary mt-3">Submit</button>
 
          </form>      
-
-               
-       
-        
-     
-       </div>
+    </div>
   </footer>
+
+  <!--slogan-->
+      <div class="containe-xxl text-center">
+        <h1 class="display-4 my-5 me-5 slogan">Live your life to fullest</h1>
+      </div>
+
+<!--copyright-->
+  <section class="bg-light">
+    <div class="container-xxl text-center">
+      <p class="secondary-text">&copy Copy right to Mt.Resort.</p>
+    </div>
+  </section>
 
 
 

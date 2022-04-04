60371bbab94df7e307d05e46c3d1e1a453c1caae
linn-khant <linkhantnyinyi242@gmail.com>
2022-04-04 02:21:07 -0700

    add footer and card style

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,7 @@
 const snav = document.querySelector(".select");
 
 document.addEventListener("wheel", (e) => {
-  if (e.pageY > 900) {
+  if (e.pageY > 700) {
     snav.classList.remove("d-none");
   } else {
     snav.classList.add("d-none");
